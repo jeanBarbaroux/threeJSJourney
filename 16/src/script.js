1eb8ed05f5486a61134cc623eec4b91f22bab434
Jean Barbaroux <jean.barbaroux@epita.fr>
2025-11-07 00:10:10 +0100

    16: haunted house

--- 16/src/script.js
+++ 16/src/script.js
@@ -18,13 +18,29 @@ const scene = new THREE.Scene()
 /**
  * House
  */
-// Temporary sphere
+const textureLoader = new THREE.TextureLoader()
+
+const alpha = textureLoader.load('./door/alpha.jpg')
+const ambientOcclusion = textureLoader.load('./door/ambientOcclusion.jpg')
+const color = textureLoader.load('./door/color.jpg')
+const height = textureLoader.load('./door/height.jpg')
+const metalness = textureLoader.load('./door/metalness.jpg')
+const normal = textureLoader.load('./door/normal.jpg')
+const roughness = textureLoader.load('./door/roughness.jpg')
+
+const floorAlpha = textureLoader.load('./floor/alpha.jpg')
+
+color.colorSpace = THREE.SRGBColorSpace
 
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshStandardMaterial({ color: "#aaff88" })
+    new THREE.MeshStandardMaterial({
+        alphaMap: floorAlpha,
+        transparent: true
+    })
 )
 floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
 const house = new THREE.Group()
 const walls = new THREE.Mesh(
@@ -40,11 +56,62 @@ const roof = new THREE.Mesh(
 roof.position.y = 3.2
 roof.rotation.y = Math.PI * 0.25
 
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2),
+    new THREE.MeshStandardMaterial({
+        map: color,
+        transparent: true,
+        alphaMap: alpha,
+        aoMap: ambientOcclusion,
+        metalnessMap: metalness,
+        normalMap: normal,
+        roughnessMap: roughness
+    })
+)
+door.position.z = 2.01
+door.position.y += 0.93
+scene.add(door)
+
 house.add(walls)
 house.add(roof)
 scene.add(house)
 
-scene.add(floor)
+const graveGeometry = new THREE.BoxGeometry(0.75, 1, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial()
+
+for (let i = 0; i < 30; i++) {
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.y += Math.random() * 0.4
+    const angle = Math.random() * Math.PI * 2
+    const position = Math.random() * 7 + 3
+    grave.position.x = Math.cos(angle) * position
+    grave.position.z = Math.sin(angle) * position
+    grave.rotation.x = (Math.random() - 0.5) * 0.6
+    grave.rotation.y = (Math.random() - 0.5) * 0.6
+    grave.rotation.z = (Math.random() - 0.5) * 0.6
+    scene.add(grave)
+}
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
+scene.add(bush1, bush2, bush3, bush4)
 
 /**
  * Lights
@@ -55,8 +122,11 @@ scene.add(ambientLight)
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
-directionalLight.position.set(3, 2, -8)
+directionalLight.position.set(3, 2, 8)
 scene.add(directionalLight)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+scene.add(directionalLightHelper)
 
 /**
  * Sizes
