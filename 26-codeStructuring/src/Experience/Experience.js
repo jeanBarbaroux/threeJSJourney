015bf77dc3de04d399d3ccc157f3b6e66e289879
Jean Barbaroux <55790634+jeanBarbaroux@users.noreply.github.com>
2025-11-24 13:41:12 +0100

    26: ajout des utils

--- 26-codeStructuring/src/Experience/Experience.js
+++ 26-codeStructuring/src/Experience/Experience.js
@@ -1,11 +1,29 @@
 import Sizes from "./utils/Size"
+import Time from "./utils/Time"
+import * as THREE from 'three'
 
 export default class Experience {
     constructor(canvas) {
         //permet d’acceder à la classe depuis la console, pas hyper utile a priori ?
         window.experience = this
 
-        this.__canvas = canvas
+        this.canvas = canvas
         this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+
+        //sizes resize event
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
+
+        //time tick event
+        this.time.on('tick', () => {
+            this.update()
+        })
     }
+
+    resize() {}
+
+    update() {}
 }
