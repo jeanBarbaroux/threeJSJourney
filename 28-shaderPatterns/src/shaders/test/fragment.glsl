bd3b99bbe7950a5ae78b3e4a715fd00b614abbc9
Jean Barbaroux <jean.barbaroux@epita.fr>
2025-12-07 21:05:41 +0100

    ajout de pattern jusqu’au 22

--- 28-shaderPatterns/src/shaders/test/fragment.glsl
+++ 28-shaderPatterns/src/shaders/test/fragment.glsl
@@ -1,5 +1,9 @@
 varying vec2 vUv;
 
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
 void main()
 {
       //pattern3
@@ -24,10 +28,56 @@ void main()
 //    float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
 
     //pattern 10
-    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+//    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
 
     //pattern 11
+//    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+//    strength += step(0.8, mod(vUv.y * 10.0, 1.0));
 
+    //pattern 12
+//    float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
+//    strength *= step(0.8, mod(vUv.x * 10.0, 1.0));
+
+    //pattern 13
+//    float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+//    barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+//    float barY = step(0.8, mod(vUv.x * 10.0, 1.0));
+//    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+//    float stregth = barX + barY;
+
+    //pattern 14
+//    float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+//    barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+//    float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+//    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+//    float strength = barX + barY;
+
+    //pattern 16
+    //float strength = abs(vUv.x - 0.5);
+
+    //pattern 17
+//    float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    //pattern 18
+    //float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    //pattern 19
+//    float strength = step(0.2,max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    //pattern 20
+//    float square1 = step(0.2,max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+//    float square2 = 1.0 - step(0.25,max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+//    float strength = square1 * square2;
+
+    //pattern 21
+//    float strength = floor(vUv.x * 10.0) / 10.0;
+
+    //pattern 22
+//    float strength = floor(vUv.x * 10.0) / 10.0;
+//    strength *= floor(vUv.y * 10.0) / 10.0;
+
+    //pattern 23
+    float strength = random(vUv);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
