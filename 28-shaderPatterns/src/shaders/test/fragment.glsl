45f8218d43d6bd19b728056f4cf031e875fbab19
Jean Barbaroux <55790634+jeanBarbaroux@users.noreply.github.com>
2025-12-08 13:57:47 +0100

    ajout de nouveaux patterns de shaders (30 à 36) avec fonction de rotation

--- 28-shaderPatterns/src/shaders/test/fragment.glsl
+++ 28-shaderPatterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,17 @@
+#define PI 3.1415926535897932384626433832795
+
 varying vec2 vUv;
 
 float random(vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+    cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+    cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
 }
 
 void main()
@@ -105,7 +115,36 @@ void main()
 //    float strength = 1.0 - distance(vUv, vec2(0.5));
 
     //pattern 29
-    float strength = 0.015 / distance(vUv, vec2(0.5));
+//    float strength = 0.015 / distance(vUv, vec2(0.5));
+
+    //pattern 30
+//    float strength = 0.15 / distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5));
+
+    //pattern 31
+//    float strength = 0.15 / distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5));
+//    strength *= 0.15 / distance(vec2((vUv.x - 0.5) * 5.0 + 0.5, vUv.y), vec2(0.5));
+
+    //pattern 32
+//    vec2 rotatedUV = rotate(vUv, PI * 0.25, vec2(0.5));
+//    vec2 lightUvX = vec2(rotatedUV.x * 0.1 + 0.45, rotatedUV.y * 0.5 + 0.25);
+//    float lightX = 0.015 / distance(lightUvX, vec2(0.5));
+//
+//    vec2 lightUvY = vec2(rotatedUV.y * 0.1 + 0.45, rotatedUV.x * 0.5 + 0.25);
+//    float lightY = 0.015 / distance(lightUvY, vec2(0.5));
+//
+//    float strength = lightX * lightY;
+
+    //pattern 33
+//    float strength = step(0.3,distance(vUv, vec2(0.5)));
+
+    //pattern 34
+//    float strength = abs(distance(vUv, vec2(0.5)) - 0.25);
+
+    //pattern 35
+//    float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+
+    //pattern 36
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
